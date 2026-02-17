52db785bb36a00db83cb343478d1462a42dea6ad
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2026-02-17 13:20:30 +0700

    update

--- fine_tune_tt100k.py
+++ fine_tune_tt100k.py
@@ -50,11 +50,11 @@ def train():
     test_dataset = Dataset(root=PATH_DATA, transforms=data_transforms, split='test')
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=WORKERS)
 
-    checkpoint = torch.load('best_checkpoint.pth', map_location=DEVICE, weights_only=True)
-    keys_to_exclude = ['classifier.classifier.head.weight', 'classifier.classifier.head.bias']
-    state_dict = {k: v for k, v in checkpoint['state_dict'].items() if k not in keys_to_exclude}
+    # checkpoint = torch.load('best_checkpoint.pth', map_location=DEVICE, weights_only=True)
+    # keys_to_exclude = ['classifier.classifier.head.weight', 'classifier.classifier.head.bias']
+    # state_dict = {k: v for k, v in checkpoint['state_dict'].items() if k not in keys_to_exclude}
     model = MambaClassifier(dims=3, depth=DEEP, num_classes=151).to(DEVICE)
-    model.load_state_dict(state_dict, strict=False)
+    # model.load_state_dict(state_dict, strict=False)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
     criterion = torch.nn.CrossEntropyLoss()
