bb9e349d1e311ef1b5d009481ba3442e0fe102c8
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2026-03-07 23:20:51 +0700

    fix

--- train_detection.py
+++ train_detection.py
@@ -98,6 +98,8 @@ def train():
                 targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
 
                 outputs = model(images, targets)
+                print(f"pred: {outputs[0]['boxes'][0]}")  # Xem tọa độ dự đoán đầu tiên
+                print(f"gt: {targets[0]['boxes'][0]}")  # Xem tọa độ nhãn thật đầu tiên
 
                 metric.update(outputs, targets)
 
